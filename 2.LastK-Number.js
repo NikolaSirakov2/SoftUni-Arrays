9732b8a398b6deb4fc135f38ccfe56b8889c8165
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-03 14:05:55 +0200

    Update 2.LastK-Number.js

--- 2.LastK-Number.js
+++ 2.LastK-Number.js
@@ -1,5 +1,12 @@
 function lastKNumber(n, k){
+    let arr = [1, 1, 2];
 
+    for (i = 2; i < k; i++){
+        let num = arr[i] + arr[i - 1] + arr[i - 2];
+        console.log(num);
+    }
+
+    // console.log(arr);
 }
 
 lastKNumber(6, 3)
