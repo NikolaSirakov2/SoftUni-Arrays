bebf63c145317ec1808c82458f9c5072af2c7206
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-04 17:30:38 +0200

    Create E05.ExtractIncreasingSubset.js

---
+++ E05.ExtractIncreasingSubset.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+    let newArr = [];
+    newArr.push(arr[0]);
+
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] >= newArr[newArr.length -1]){
+            newArr.push(arr[i])
+        }
+    }
+
+    return newArr;
+}
+
+solve([20, 3, 2, 15, 6, 1
+]);
